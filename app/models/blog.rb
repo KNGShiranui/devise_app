ac15b2b818939c1e5972388dee619a5d128e7f9f
kengo <mathematics9973@gmail.com>
2023-03-15 16:03:22 +0900

    public_private_switch

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,7 @@
 class Blog < ApplicationRecord
+  enum status: { public: 0, private: 1 }, _prefix: true
+  # 上記のように_prefix: trueを記述してない状態でブラウザを開いたら、エラーが出ることがある。
+  # エラー文を確認すると、publicというメソッドが重複しているとのこと。
+  # 重複してエラーが出ていなければ_prefix: trueを記述しなくてもOK。
+  belongs_to :user
 end
